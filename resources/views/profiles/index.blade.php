4ad856c4e4f64976eb7e152a62a6d58ee9a6984d
Arpit Jain <arpit.jain@giift.com>
2020-08-19 23:54:17 +0530

    Authorisation handling with policy added

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,13 +4,20 @@
 <div class="container">
     <div class="row">
         <div class="col-md-3 p-5">
-            <img class="rounded-circle" src="https://instagram.fjai1-2.fna.fbcdn.net/v/t51.2885-19/s150x150/88947585_531372544175761_1507515975954595840_n.jpg?_nc_ht=instagram.fjai1-2.fna.fbcdn.net&_nc_ohc=ul2UC38J2LkAX_AuS78&oh=acaecaf4bb4732d2b2cb6740ac52c7d9&oe=5F5F652B">
+            <img class="rounded-circle w-100" src="/storage/{{ $user->profile->image }}">
         </div>
         <div class="col-md-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username }}</h1>
-                <a href="/p/create">Add New Post</a>
+                @can('update', $user->profile)
+                    <a href="/p/create">Add New Post</a>
+                @endcan
             </div>
+
+            @can('update', $user->profile)
+                <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+            @endcan
+
             <div class="d-flex">
                 <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
